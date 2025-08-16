238568b80603b00573ad6890708afef6bd4e11e0
Jorge Kean <jkn379@gmail.com>
2025-08-16 12:07:01 +0800

    add github actions

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
       // Optionally, configure caching strategies and other service worker options here.
     }),
   ],
+  base: '/agos/',
   server: {
     open: true,
   },
