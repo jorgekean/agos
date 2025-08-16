15c4e0ba3c07505f10f5d429dd40f5b4e1f56537
Jorge Kean <jkn379@gmail.com>
2025-08-16 09:20:27 +0800

    copy from main

---
+++ src/models/BillingManager.ts
@@ -0,0 +1,11 @@
+export interface BillingManagerModel {
+  id?: string
+  client: string
+  projectCode: string
+  taskCode: string
+  billingType?: "Billable" | "NonBillable"
+  allocatedHours?: number
+  actualHours?: number
+  isArchived?: boolean
+  isSynced: number 
+}
